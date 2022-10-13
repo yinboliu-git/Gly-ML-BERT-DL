1797532cfcf0cbbb49c13c935139e3f2b7f06a9e
Yinbo Liu <92346875+yinboliu-git@users.noreply.github.com>
2022-10-13 22:32:35 +0800

    Update extral_feature.sh

--- Bert_extraction/extral_feature.sh
+++ Bert_extraction/extral_feature.sh
@@ -1,11 +1,7 @@
-# /mnt/raid5/data3/xlzhu/learn_DL/BERT_pretrain/model_pt/multi_cased_L-12_H-768_A-12
-# /mnt/raid5/data3/xlzhu/learn_DL/BERT_pretrain/model_pt/probert
-export BERT_BASE_DIR=/mnt/raid5/data3/xlzhu/learn_DL/BERT_pretrain/model_pt/probert
-#export BERT_BASE_DIR=/mnt/raid5/data3/xlzhu/learn_DL/BERT_pretrain/model_pt/multi_cased_L-12_H-768_A-12
-
-python3 /mnt/raid5/data3/xlzhu/learn_DL/BERT_pretrain/bert/extract_features.py \
-    --input_file=/mnt/raid5/data4/publice/gly_site_pred/test_data/fold10/valid.txt \
-    --output_file=/mnt/raid5/data4/publice/gly_site_pred/probert_fea/source_bert_data/fold10/valid.json \
+BERT_BASE_DIR=your_bert_file
+python3 ./bert_bash/extract_features.py \
+    --input_file=../data/dataset/fold10/valid.txt \
+    --output_file=../data/dataset/valid.json \
     --layers=-1 \
     --vocab_file=$BERT_BASE_DIR/vocab.txt \
     --bert_config_file=$BERT_BASE_DIR/bert_config.json \
